1db93aeee48fee9cfb51f2c01aa3b0ec63c8978f
Minhtri-hub <nguyenquachminhtri@gmail.com>
2026-03-10 15:07:59 +0700

    Add files via upload

---
+++ list-product/list-product.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-product',
+  standalone: false,
+  templateUrl: './list-product.html',
+  styleUrl: './list-product.css',
+})
+export class ListProduct {
+  products = ['Coca', 'Pepsi', 'Sting'];
+}
+
---
+++ product/product.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: false,
+  templateUrl: './product.html',
+  styleUrl: './product.css',
+})
+export class Product {}
+
